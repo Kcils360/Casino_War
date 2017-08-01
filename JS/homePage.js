de7a37c5df83c0345d13ae5c612df2c91565378b
Gregory Dukes <kcils360@gmail.com>
2017-08-01 15:07:35 -0700

    linked homepage and about us, styled

--- JS/homePage.js
+++ JS/homePage.js
@@ -1,29 +1,14 @@
 'use strict';
 
 
-// var userInfoArray = [];
-
 function initiateGame(e){
   e.preventDefault();
   var username = document.getElementById('username').value;
   var betAmount = document.getElementById('betAmount').value;
   console.log(username, ' bet ', betAmount);
-  // userInfoArray.push(username);
-  // userInfoArray.push(betAmount);
   localStorage.setItem('username', username);
   localStorage.setItem('betAmount', betAmount);
-}
-
-
-
-
-
-
-
-
-
-
-// cartFormArray = JSON.parse(localStorage.userFormInfo);
+};
 
 
 
