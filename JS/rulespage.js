7877e6e472457b5a09710b2eb1f37e62576f3430
Gregory Dukes <kcils360@gmail.com>
2017-08-02 15:22:09 -0700

    fixed sign in issues

--- JS/rulespage.js
+++ JS/rulespage.js
@@ -1,9 +1,14 @@
 'use strict';
 
-localStorage.clear();
+var userName = localStorage.gambler;
+
 function saveBet(e){
   e.preventDefault();
   var betAmount = document.getElementById('betAmount').value;
   localStorage.setItem('bank', parseInt(betAmount));
 }
 document.getElementById('play').addEventListener('click', saveBet);
+
+if(userName.length > 0){
+  document.getElementById('username').textContent = userName;
+}
